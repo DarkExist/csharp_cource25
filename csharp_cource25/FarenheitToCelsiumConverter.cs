3f7b46a425bca3a186fca4e1bf23e626114b6716
relog <relogotech@gmail.com>
2025-04-08 20:02:33 +0700

    solved

---
+++ csharp_cource25/FarenheitToCelsiumConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_cource25
+{
+    public static class FarenheitToCelsiumConverter
+    {
+        public static double Convert(double farenheitValue)
+        {
+			return Math.Round((farenheitValue - 32) * (5.0 / 9), 4);
+        }
+    }
+}

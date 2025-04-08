c654d43903155559ffc939633e3a69bc73f7e6c9
relog <relogotech@gmail.com>
2025-04-08 20:29:34 +0700

    solved

---
+++ csharp_cource25/NubmerBooleaner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_cource25
+{
+    public static class NubmerBooleaner
+    {
+        public static bool IsAandBOdd(BigInteger a, BigInteger b)
+        {
+            return !a.IsEven && !b.IsEven;
+        }
+    }
+}

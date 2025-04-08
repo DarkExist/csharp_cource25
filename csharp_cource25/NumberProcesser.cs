11e676ca838c116dda0b9dd3e0c45d6eb8f040ee
relog <relogotech@gmail.com>
2025-04-08 20:40:25 +0700

    solved

---
+++ csharp_cource25/NumberProcesser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_cource25
+{
+    public static class NumberProcesser
+    {
+        public static BigInteger SumOfTwo( BigInteger[] numbers )
+        {
+            Array.Sort(numbers);
+            return numbers[^1] + numbers[^2];
+        }
+    }
+}

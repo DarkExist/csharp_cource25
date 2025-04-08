6f5ac8a591fd57d98604ceb1959a456bb39ccc5a
relog <relogotech@gmail.com>
2025-04-08 19:54:36 +0700

    Добавьте файлы проекта.

---
+++ csharp_cource25/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp_cource25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

5a02211f649b6b6bccf62bd442b41d5df84ec08d
relog <relogotech@gmail.com>
2025-04-09 08:48:42 +0700

    solved

---
+++ csharp_cource25/ForProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_cource25
+{
+    public static class ForProvider
+    {
+        public static BigInteger SumOfSquaresFromAtoB(BigInteger a, BigInteger b)
+        {
+            BigInteger sum = BigInteger.Zero;
+
+            for (BigInteger integer = a; integer <= b; integer++)
+            {
+                sum += integer * integer;
+            }
+            return sum;
+        }
+    }
+}
